e78a008fe747d8afd39d669673d24fbfb29aafc7
linzhixiang <615984601@qq.com>
2017-12-26 14:46:52 +0800

    log4 test

--- src/main/java/org/springboot/sample/runner/MyStartupRunner1.java
+++ src/main/java/org/springboot/sample/runner/MyStartupRunner1.java
@@ -1,5 +1,8 @@
 package org.springboot.sample.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springboot.sample.SpringBootSampleApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -19,6 +22,12 @@ public class MyStartupRunner1 implements CommandLineRunner {
    
     public void run(String... args) throws Exception {
         System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作1111111111111<<<<<<<<<<<<<");
+     // 在Java类中创建 logger 实例
+        final Logger logger = LoggerFactory.getLogger(SpringBootSampleApplication.class);
+        // 在方法中使用日志输出，如
+        
+        logger.debug("日志输出测试 Debug");
+        logger.trace("日志输出测试 Trace");
+        logger.info("日志输出测试 Info");
     }
-
 }
